d8d8c43b468370179e0a9a6341fbb4119a21b67c
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2024-11-10 20:20:36 +0300

    (add) 242_Valid_Anagram.php

---
+++ Arrays_Hashing/242_Valid_Anagram.php
@@ -0,0 +1,37 @@
+<?php
+
+// Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+// Учитывая две строки s и t, верните true, если эти две строки являются анаграммами друг друга, в противном случае верните false.
+
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+// Анаграмма — это строка, содержащая те же символы, что и другая строка, но порядок символов может быть другим.
+
+function isAnagram($s, $t) {
+    if (strlen($s) !== strlen($t)) {
+        return false;
+    }
+
+    $sChars = [];
+    $tChars = [];
+
+    for ($i = 0; $i < strlen($s); $i++) {
+        $sChar = $s[$i];
+        if (array_key_exists($sChar, $sChars)) {
+            $sChars[$sChar] += 1;
+        } else {
+            $sChars[$sChar] = 1;
+        }
+
+        $tChar = $t[$i];
+        if (array_key_exists($tChar, $tChars)) {
+            $tChars[$tChar] += 1;
+        } else {
+            $tChars[$tChar] = 1;
+        }
+    }
+
+    return $sChars == $tChars;
+}
+
+$res = isAnagram("racecar", "carrace");
+var_dump($res);
